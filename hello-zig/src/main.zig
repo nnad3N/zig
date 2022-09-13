592ee2fa25fcd136ea3df39811ca93ff431e609d
Damian Struk <nnad3n@outlook.com>
2022-09-13 19:03:59 +0200

    minor opts

--- hello-zig/src/main.zig
+++ hello-zig/src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-fn read_int(message: []const u8) !i64 {
+fn read_int(message: []const u8) !u64 {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
 
@@ -9,19 +9,19 @@ fn read_int(message: []const u8) !i64 {
     try stdout.print("{s}", .{message});
 
     if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |input| {
-        return std.fmt.parseInt(i64, input, 10);
+        return std.fmt.parseInt(u64, input, 10);
     } else {
-        return @as(i64, 0);
+        return @as(u64, 0);
     }
 }
 
 pub fn main() !void {
     const limit = try read_int("How many of prime numbers do you need: ");
-    var i: i64 = 2;
+    var i: u64 = 2;
     while (i <= limit ) : (i += 1) {
-        var j: i64 = 2;
-            while (j <= i) : (j += 1) {
-                if (@mod(i, j) == 0) {
+        var j: u64 = 2;
+            while (j <= i/2) : (j += 1) {
+                if (i % j == 0) {
                 break;
                 } else {
                     std.log.info("{d}", .{i});
